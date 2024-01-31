34a9895eead4be8b4665291beb77c23e8ff4fe7e
Potowai <alexis.fiolleau@ecoles-epsi.net>
2024-01-31 16:34:52 +0100

    :white_check_mark: step2 hello !!

--- src/finder.py
+++ src/finder.py
@@ -4,7 +4,7 @@ import langs
 
 def is_pal(chaine, lang=langs.Francais):
     chaine = chaine[::-1]
-    chaine = "Bonjour" + os.linesep + chaine
+    chaine = lang.hello() + os.linesep + chaine
     if chaine == chaine[::1]:
         chaine += os.linesep + lang.well_sayed()
     chaine += os.linesep + "Au revoir!"
--- src/langs.py
+++ src/langs.py
@@ -3,8 +3,16 @@ class Francais:
     def well_sayed():
         return "Bien dit!"
 
+    @staticmethod
+    def hello():
+        return "Bonjour"
+
 
 class English:
     @staticmethod
     def well_sayed():
         return "Well sayed!"
+
+    @staticmethod
+    def hello():
+        return "Hello"

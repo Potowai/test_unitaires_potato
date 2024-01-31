488e0c28e9f4a788265444f46478359d1a4731a8
Potowai <alexis.fiolleau@ecoles-epsi.net>
2024-01-31 16:02:04 +0100

    :white_check_mark: step1.4

--- src/finder.py
+++ src/finder.py
@@ -2,7 +2,9 @@ import os
 
 
 def is_pal(chaine):
+    chaine = chaine[::-1]
     chaine = "Bonjour" + os.linesep + chaine
     if chaine == chaine[::1]:
-        return chaine + os.linesep + "Bien dit!"
-    return chaine[::-1]
+        chaine += os.linesep + "Bien dit!"
+    chaine += os.linesep + "Au revoir!"
+    return chaine
